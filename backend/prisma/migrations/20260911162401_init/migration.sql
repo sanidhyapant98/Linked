29dfbeac4d3cce27831f76e910927c73f9615def
sandy <152873379+sanidhyapant98@users.noreply.github.com>
2026-09-12 03:08:14 +0530

    feat: add initial Prisma schema and migration files for Link model

---
+++ backend/prisma/migrations/20260911162401_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "Link" (
+    "id" SERIAL NOT NULL,
+    "shortCode" TEXT NOT NULL,
+    "originalUrl" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Link_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Link_shortCode_key" ON "Link"("shortCode");
